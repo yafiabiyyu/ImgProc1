770b2948da7184ecc4b54810a3168d4b17f05856
yafiabiyyu <luckyveri3@gmail.com>
2020-02-18 03:21:32 +0700

    Add project files.

---
+++ ImgProc1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ImgProc1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pbInput = new System.Windows.Forms.PictureBox();
+            this.pbOutput = new System.Windows.Forms.PictureBox();
+            this.bukaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simpanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keluarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.averageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightnessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.luminanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.colorsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1084, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bukaToolStripMenuItem,
+            this.simpanToolStripMenuItem,
+            this.keluarToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(42, 20);
+            this.toolStripMenuItem1.Text = "Files";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 439);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1084, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pbInput
+            // 
+            this.pbInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbInput.Location = new System.Drawing.Point(25, 47);
+            this.pbInput.Name = "pbInput";
+            this.pbInput.Size = new System.Drawing.Size(500, 350);
+            this.pbInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbInput.TabIndex = 2;
+            this.pbInput.TabStop = false;
+            // 
+            // pbOutput
+            // 
+            this.pbOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOutput.Location = new System.Drawing.Point(572, 47);
+            this.pbOutput.Name = "pbOutput";
+            this.pbOutput.Size = new System.Drawing.Size(500, 350);
+            this.pbOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOutput.TabIndex = 3;
+            this.pbOutput.TabStop = false;
+            // 
+            // bukaToolStripMenuItem
+            // 
+            this.bukaToolStripMenuItem.Name = "bukaToolStripMenuItem";
+            this.bukaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bukaToolStripMenuItem.Text = "&Buka";
+            this.bukaToolStripMenuItem.Click += new System.EventHandler(this.bukaToolStripMenuItem_Click);
+            // 
+            // simpanToolStripMenuItem
+            // 
+            this.simpanToolStripMenuItem.Name = "simpanToolStripMenuItem";
+            this.simpanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.simpanToolStripMenuItem.Text = "&Simpan sebagai...";
+            this.simpanToolStripMenuItem.Click += new System.EventHandler(this.simpanToolStripMenuItem_Click);
+            // 
+            // keluarToolStripMenuItem
+            // 
+            this.keluarToolStripMenuItem.Name = "keluarToolStripMenuItem";
+            this.keluarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.keluarToolStripMenuItem.Text = "&Keluar";
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayscaleToolStripMenuItem,
+            this.averageToolStripMenuItem,
+            this.lightnessToolStripMenuItem,
+            this.luminanceToolStripMenuItem});
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grayscaleToolStripMenuItem.Text = "&Grayscale";
+            // 
+            // averageToolStripMenuItem
+            // 
+            this.averageToolStripMenuItem.Name = "averageToolStripMenuItem";
+            this.averageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.averageToolStripMenuItem.Text = "&Average";
+            // 
+            // lightnessToolStripMenuItem
+            // 
+            this.lightnessToolStripMenuItem.Name = "lightnessToolStripMenuItem";
+            this.lightnessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lightnessToolStripMenuItem.Text = "&Lightness";
+            // 
+            // luminanceToolStripMenuItem
+            // 
+            this.luminanceToolStripMenuItem.Name = "luminanceToolStripMenuItem";
+            this.luminanceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.luminanceToolStripMenuItem.Text = "&Luminance";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 461);
+            this.Controls.Add(this.pbOutput);
+            this.Controls.Add(this.pbInput);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Garuda Image Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.PictureBox pbInput;
+        private System.Windows.Forms.PictureBox pbOutput;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem bukaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simpanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keluarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem averageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightnessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem luminanceToolStripMenuItem;
+    }
+}
+
---
+++ ImgProc1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgProc1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bukaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog bukaFile = new OpenFileDialog();
+            bukaFile.Filter = "Image File (*.bmp, *.jpg, *.jpeg) | *.bmp;*.jpg;*.jpeg";
+            if(DialogResult.OK == bukaFile.ShowDialog())
+            {
+                this.pbInput.Image = new Bitmap(bukaFile.FileName);
+            }
+        }
+
+        private void simpanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(pbOutput.Image == null)
+            {
+                MessageBox.Show("Tidak Ada citra yang akan disimpan");
+            }else
+            {
+                SaveFileDialog simpanFile = new SaveFileDialog();
+                simpanFile.Title = "Simpan File Citra";
+                simpanFile.Filter = "Image File (*.bmp, *.jpg, *.jpeg) | *.bmp;*.jpg;*.jpeg";
+                if(DialogResult.OK == simpanFile.ShowDialog())
+                {
+                    this.pbOutput.Image.Save(simpanFile.FileName);
+                }
+            }
+        }
+    }
+}
